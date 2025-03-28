baffe00889e761a9f2ab020d087dabb911375db0
Impulse <ephraimimpulse17@gmail.com>
2025-03-28 16:13:12 +0100

    ajout de la fonctionnalité d'inscription

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   async loginUser(email: string, password: string): Promise<User> {
     const user = {
       email: email,
@@ -18,6 +17,29 @@ export class UserService {
 
       body: JSON.stringify(user),
     }).then((response) => response.json());
+    console.log(rep);
+
+    return rep;
+  }
+
+  async registerUser(
+    email: string,
+    name: string,
+    password: string,
+    password_confirmation: string
+  ): Promise<User> {
+    const user = {
+      email: email,
+      name: name,
+      password: password,
+      password_confirmation: password_confirmation,
+    };
+    let rep = await fetch('http://localhost:8000/api/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    }).then((response) => response.json());
+
     console.log(rep);
 
     return rep;
