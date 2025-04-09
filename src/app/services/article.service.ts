53c4c16ecfdc871df50ff2f8ed78d1ebe1ad72fd
impulse <ephraimimpulse17@gmail.com>
2025-04-09 18:21:19 +0100

    Resolution Bug pagination et getTreeLatestArticle

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -11,8 +11,7 @@ export class ArticleService {
   articles: ArticleApi[] = [];
   comments: Comment[] = [];
 
-  //url = inject(BASE_URL);
-  url = "http://127.0.0.1:8000"
+  url = inject(BASE_URL);
 
   //avec resource en collection
   async getAll(link?: string) {
