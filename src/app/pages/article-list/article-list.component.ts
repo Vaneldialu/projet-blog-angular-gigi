5bbf6b441ef2a00bd0860a97105a8875f7d05a25
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-04-09 18:40:47 +0100

    Resolution du bug lié a la pagination

--- src/app/pages/article-list/article-list.component.ts
+++ src/app/pages/article-list/article-list.component.ts
@@ -8,35 +8,25 @@ import { Links } from '../../models/links';
 import { Meta } from '../../models/meta';
 import { VideoComponent } from '../article-video/video.component';
 import { ArtcileVidComponent } from "../artcile-vid/artcile-vid.component";
-import { LastArticleComponent } from "../last-article/last-article.component";
 
 @Component({
   selector: 'app-article-list',
-<<<<<<< HEAD
-  imports: [ArticleComponent, NgFor, RouterLink, NgForOf, NgClass, ArtcileVidComponent, LastArticleComponent],
-=======
   imports: [ArticleComponent, NgFor, RouterLink, NgForOf, NgClass,ArtcileVidComponent, NgIf],
->>>>>>> cec8433d58a432a5910ad1e7131fa5f5295b4b39
   templateUrl: './article-list.component.html',
   standalone: true,
   styleUrl: './article-list.component.css',
 })
 export class ArticleListComponent {
-<<<<<<< HEAD
-  onRefreshPage() {
-    this.getAll();
-  }
-  articles!: ArticleApi[];
-=======
   articles: ArticleApi[] = []; // <- éviter le ! ici
->>>>>>> cec8433d58a432a5910ad1e7131fa5f5295b4b39
   service: ArticleService = inject(ArticleService);
   links?: Links;
   meta?: Meta;
   isConnected: boolean = false;
   isLoading: boolean = true; // <- Ajout
 
-  
+  onRefreshPage() {
+    this.getAll();
+  }
 
   ngOnInit() {
     this.isConnected = !!localStorage.getItem('token');
@@ -50,16 +40,11 @@ export class ArticleListComponent {
       this.articles = reponse.data;
       this.links = reponse.links;
       this.meta = reponse.meta;
-<<<<<<< HEAD
-      console.log("reponse:",this.articles);
-      
-=======
       this.isLoading = false; // <- Arrêter le loader
       console.log("reponse:", reponse);
     }).catch((error) => {
       console.error("Erreur lors du chargement des articles:", error);
       this.isLoading = false; // <- Assurer l'arrêt même en cas d'erreur
->>>>>>> cec8433d58a432a5910ad1e7131fa5f5295b4b39
     });
   }
 
